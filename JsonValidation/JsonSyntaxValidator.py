b2d0c4ab378898b1c49f8c54d622f100b0cfd929
azez0 <nanquery@gmail.com>
2023-06-09 15:46:47 +0200

    modify errors output

--- JsonValidation/JsonSyntaxValidator.py
+++ JsonValidation/JsonSyntaxValidator.py
@@ -1,13 +1,25 @@
 import os
-import subprocess
 import json5
 
 
 class JsonSyntaxValidator:
     def __init__(self, directory):
+        """
+        Initialize the JsonSyntaxValidator object.
+
+        Args:
+            directory (str): Directory path to validate JSON files.
+        """
         self.__directory = directory
+        self.__errors = []
 
     def validate_all_json_files(self):
+        """
+        Validate all JSON files in the specified directory.
+
+        Returns:
+            bool: True if all JSON files are valid, False otherwise.
+        """
         for root, dirs, files in os.walk(self.__directory):
             for file in files:
                 if file.endswith('.json'):
@@ -16,6 +28,11 @@ class JsonSyntaxValidator:
                         try:
                             json5.load(f)
                         except ValueError as e:
-                            print(f'Invalid JSON in file {file_path}: {str(e)}')
-                            return False
+                            error_msg = f'Invalid JSON in file {file_path}: {str(e)}'
+                            self.__errors.append(error_msg)
+
+        if self.__errors:
+            print(self.__errors)
+            return False
+
         return True
